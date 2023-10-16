c4066559581fc0fd8af1cb806a6f495a2f414940
wangyakun <369358123@qq.com>
2023-10-16 20:28:18 +0800

    py3调试成功，重构并加标签等功能

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # wechat_robot
 
-微信自动回复机器人
-
+## 微信自动回复机器人
 
 使用方法：
 
@@ -13,21 +12,19 @@
 
 1.使用itchat微信接口，可获取聊天内容及发送微信消息
 
-2.将获取到的聊天内容对接到图灵机器人(www.tuling123.com
-)，得到回复，发送回去
-
+2.将获取到的聊天内容对接到青云客机器人(api.qingyunke.com)，得到回复，发送回去
 
 功能：
 1.实现了控制台，可控制如下：
 
   (1)打开/关闭机器人
-  
+
   (2)对特定好友打开/关闭机器人
-  
+
   (3)打开/关闭延时回复
-  
+
   (4)打开/关闭打扰模式
-  
+
 控制台操作：
 向 文件传输助手 发送：
 
@@ -39,24 +36,29 @@
 
 “disturb open” 打开打扰模式
 
-“close” 关闭机器人
+“set label close” 关闭【机器人自动回复】标签
 
-“open” 打开机器人
+“set label open” 打开【机器人自动回复】标签
 
-“close XXX” 针对XXX屏蔽自动回复
+“close” 关闭机器人自动回复
 
-“open XXX” 针对XXX打开自动回复
+“open” 打开机器人自动回复
 
-“list” 查看屏蔽列表
+“set close XXX” 针对XXX强制关闭自动回复(XXX为昵称)
 
-“closelist XXX,XXX,XXX” 对多人屏蔽自动回复
+“set open XXX” 针对XXX强制打开自动回复
+
+“set cancel XXX” 把XXX从自动回复列表中取消
+
+“list” 查看自动回复列表（True：强制自动回复，False：强制不自动回复）
+
+"status" 查看当前状态
+
+“set closelist XXX,XXX,XXX” 对多人屏蔽自动回复
 
 “auther” 作者信息
 
-
-
-微信自动回复机器人-后台
-
+## 微信自动回复机器人-后台（暂未实现）
 
 使用方法：
 
--- selfWechatRobot.py
+++ selfWechatRobot.py
@@ -1,4 +1,4 @@
-#coding=utf8
+# coding=utf8
 __author__ = 'WYK'
 
 import os
@@ -6,8 +6,12 @@ import time
 import logging
 import requests
 import itchat
-# 关于itchat问题：AttributeError: 'HTMLParser' object has no attribute 'unescape'  解决：https://github.com/littlecodersh/ItChat/pull/932/files
-# 关于itchat问题：itchat.auto_login登录问题（1.3.10存在,  1.2不存在）  解决：https://github.com/littlecodersh/ItChat/issues/970 ：
+# 关于itchat问题：AttributeError: 'HTMLParser' object has no attribute 'unescape' （python>=3.9 存在） 解决：https://github.com/littlecodersh/ItChat/pull/932/files ：
+#       在 itchat/utils.py 的 htmlParser = HTMLParser() 后面加:
+#             if not hasattr(htmlParser, 'unescape'):
+#                 import html
+#                 htmlParser.unescape = html.unescape
+# 关于itchat问题：itchat.auto_login登录问题（itchat 1.3.10存在,  1.2不存在）  解决：https://github.com/littlecodersh/ItChat/issues/970 ：
 #       在 itchat/components/login.py 的 login() 函数中，在进入 while not isLoggedIn 循环前加了一个sleep
 import threading
 import datetime
@@ -15,49 +19,51 @@ import random
 import traceback
 
 import importlib, sys
+
 importlib.reload(sys)
 
 help_info = '''“delay close” 关闭延时回复
 “delay open” 打开延时回复
 “disturb close” 关闭打扰模式
 “disturb open” 打开打扰模式
-“close” 关闭机器人
-“open” 打开机器人
-“close XXX” 针对XXX屏蔽自动回复
-“open XXX” 针对XXX打开自动回复
-“list” 查看屏蔽列表
-“closelist XXX,XXX,XXX” 对多人屏蔽自动回复
+“set label close” 关闭【机器人自动回复】标签
+“set label open” 打开【机器人自动回复】标签
+“close” 关闭机器人自动回复
+“open” 打开机器人自动回复
+“set close XXX” 针对XXX强制关闭自动回复(XXX为昵称)
+“set open XXX” 针对XXX强制打开自动回复
+“set cancel XXX” 把XXX从自动回复列表中取消
+“list” 查看自动回复列表（True：强制自动回复，False：强制不自动回复）
+"status" 查看当前状态
+“set closelist XXX,XXX,XXX” 对多人屏蔽自动回复
 “auther” 作者信息'''
 
-# KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
-KEY = 'f26276bebeba492ab763e83e89c511d0'
-
-record_log = True
-auto_rep = True
-no_auto_rep_list = []
-time_interval = True
-append_disturb = True
+is_delay_rep = False
+is_disturb = False
+is_robot_label_display = True
 last_repl_time = datetime.datetime.now()
-pic_repl_list = [u'[发呆]', u'[呲牙]', u'[愉快]', u'[偷笑]', u'[憨笑]', u'[抠鼻]', u'[坏笑]', u'[阴险]', u'[嘿哈]', u'[奸笑]', u'[机智]']
+pic_repl_list = [u'[发呆]', u'[呲牙]', u'[愉快]', u'[偷笑]', u'[憨笑]', u'[抠鼻]', u'[坏笑]', u'[阴险]', u'[嘿哈]',
+                 u'[奸笑]', u'[机智]']
+
 
 class LOG:
     def __init__(self):
         self.LOG_HOME = r'D:\log\wxRobotLog'
-        self.NO_AUTO_REP_LIST = r'D:\log\noAutoRepList'
+        self.is_record_log = True
         if not os.path.exists(self.LOG_HOME):
             os.makedirs(self.LOG_HOME)
         logging.basicConfig(level=logging.DEBUG,
-            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-            datefmt='%a, %d %b %Y %H:%M:%S',
-            filename=os.path.join(self.LOG_HOME, 'wxRobot.log'),
-            filemode='w')
+                            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                            datefmt='%a, %d %b %Y %H:%M:%S',
+                            filename=os.path.join(self.LOG_HOME, 'wxRobot.log'),
+                            filemode='w')
         self.set_file('default')
 
     def set_file(self, user):
         self.user = user
 
     def log(self, message):
-        if not record_log:
+        if not self.is_record_log:
             return
         now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         print(now)
@@ -69,86 +75,138 @@ class LOG:
             f.write(message)
             f.write('\n')
 
+
+class RepListManager:
+    def __init__(self):
+        self.is_defualt_auto_rep = False
+        self.NO_AUTO_REP_LIST_FILE = r'D:\log\noAutoRepList'
+        self.auto_rep_dict = {}
+
+    def set_auto_rep(self, user, is_rep):
+        if user is None:
+            return
+        self.auto_rep_dict[user] = is_rep
+        self.save_no_auto_rep_list()
+
+    def judge_auto_rep(self, user):
+        return self.auto_rep_dict.get(user) if self.auto_rep_dict.get(user) is not None else self.is_defualt_auto_rep
+
+    def remove_auto_rep(self, user):
+        if user in self.auto_rep_dict:
+            self.auto_rep_dict.pop(user)
+
     def save_no_auto_rep_list(self):
-        global no_auto_rep_list
-        no_auto_rep_list = list(set(no_auto_rep_list))
-        with open(self.NO_AUTO_REP_LIST, 'w') as f:
-            f.write(str(no_auto_rep_list))
+        with open(self.NO_AUTO_REP_LIST_FILE, 'w') as f:
+            f.write(str(self.auto_rep_dict))
 
     def load_no_auto_rep_list(self):
-        global no_auto_rep_list
-        if not os.path.exists(self.NO_AUTO_REP_LIST):
+        if not os.path.exists(self.NO_AUTO_REP_LIST_FILE):
             return False
-        with open(self.NO_AUTO_REP_LIST, 'r') as f:
-            no_auto_rep_list = list(f.read())
+        with open(self.NO_AUTO_REP_LIST_FILE, 'r') as f:
+            self.auto_rep_dict = dict(eval(f.read()))
+
 
 logger = LOG()
+rep_mgr = RepListManager()
 
-def ctl_msg(msg):
-    global auto_rep, no_auto_rep_list, time_interval, append_disturb, record_log
-    if msg['ToUserName'] != 'filehelper':
+def msg_with_label(msg_text):
+    if is_robot_label_display:
+        msg_text = u'【机器人自动回复】' + msg_text
+    return msg_text
+
+def send_msg(msg_text, user):
+    itchat.send(msg_with_label(msg_text), user)
+
+
+def ctl_msg(msg_info):
+    global logger, rep_mgr, is_delay_rep, is_disturb, is_robot_label_display
+    if msg_info['ToUserName'] != 'filehelper':
         return False
     print('!!!this is admin!!!')
     print('msg:')
-    print(msg['Text'])
+    print(msg_info['Text'])
 
-    if msg['Text'] == 'open':
-        auto_rep = True
-        itchat.send(u'机器人已开启', 'filehelper')
-    elif msg['Text'].startswith('open '):
-        user = msg['Text'][5:]
-        if user in no_auto_rep_list:
-            no_auto_rep_list.remove(user)
-        itchat.send(u'机器人已对 %s 开启' % user, 'filehelper')
-    elif msg['Text'] == 'close':
-        auto_rep = False
-        itchat.send(u'机器人已关闭', 'filehelper')
-    elif msg['Text'].startswith('closelist '):
-        close_list = msg['Text'][11:].split(',')
-        no_auto_rep_list.extend(close_list)
-        logger.save_no_auto_rep_list()
-        itchat.send(u'机器人已对列表 %s 关闭' % close_list, 'filehelper')
-    elif msg['Text'].startswith('close '):
-        user = msg['Text'][6:]
-        if user not in no_auto_rep_list:
-            no_auto_rep_list.append(user)
-            logger.save_no_auto_rep_list()
-        itchat.send(u'机器人已对 %s 关闭' % user, 'filehelper')
-    elif msg['Text'] == 'list':
-        print(no_auto_rep_list)
-        itchat.send(u'已关闭列表：', 'filehelper')
-        itchat.send(','.join(no_auto_rep_list), 'filehelper')
-    elif msg['Text'].startswith('delay '):
-        ctrl = msg['Text'][6:]
+
+    if msg_info['Text'] == 'open':
+        rep_mgr.is_defualt_auto_rep = True
+        send_msg(u'机器人已开启', 'filehelper')
+    elif msg_info['Text'] == 'close':
+        rep_mgr.is_defualt_auto_rep = False
+        send_msg(u'机器人已关闭', 'filehelper')
+    elif msg_info['Text'].startswith('set open '):
+        user = msg_info['Text'][9:]
+        rep_mgr.set_auto_rep(user, True)
+        send_msg(u'机器人已对 %s 开启' % user, 'filehelper')
+    elif msg_info['Text'].startswith('set close '):
+        user = msg_info['Text'][10:]
+        rep_mgr.set_auto_rep(user, False)
+        send_msg(u'机器人已对 %s 关闭' % user, 'filehelper')
+    elif msg_info['Text'].startswith('set openlist '):
+        open_list = msg_info['Text'][14:].split(',')
+        for user in open_list:
+            rep_mgr.set_auto_rep(user, True)
+        send_msg(u'机器人已对列表 %s 开启' % open_list, 'filehelper')
+    elif msg_info['Text'].startswith('set closelist '):
+        close_list = msg_info['Text'][15:].split(',')
+        for user in close_list:
+            rep_mgr.set_auto_rep(user, False)
+        send_msg(u'机器人已对列表 %s 关闭' % close_list, 'filehelper')
+    elif msg_info['Text'].startswith('set cancel '):
+        user = msg_info['Text'][11:]
+        rep_mgr.remove_auto_rep(user)
+        send_msg(u'机器人已对 %s 从自动回复列表中删除' % user, 'filehelper')
+    elif msg_info['Text'].startswith('set label '):
+        ctrl = msg_info['Text'][10:]
         if ctrl == 'open':
-            time_interval = True
-            itchat.send(u'延时回复已开启', 'filehelper')
+            is_robot_label_display = True
+            send_msg(u'机器人自动回复标签已开启', 'filehelper')
         elif ctrl == 'close':
-            time_interval = False
-            itchat.send(u'延时回复已关闭', 'filehelper')
-    elif msg['Text'].startswith('disturb '):
-        ctrl = msg['Text'][8:]
+            is_robot_label_display = False
+            send_msg(u'机器人自动回复标签已关闭', 'filehelper')
+    elif msg_info['Text'] == 'list':
+        print(rep_mgr.auto_rep_dict)
+        send_msg(u'自动回复列表：\n' + str(rep_mgr.auto_rep_dict), 'filehelper')
+    elif msg_info['Text'] == 'status':
+        rep_msg = "当前状态：\n" \
+                  "机器人是否开启：%s\n" \
+                  "自动回复标签是否开启：%s\n" \
+                  "延时回复模式是否开启：%s\n" \
+                  "打扰模式（自动回复“怎么不说话了”）是否开启：%s"
+        send_msg(rep_msg % (rep_mgr.is_defualt_auto_rep, is_robot_label_display, is_delay_rep, is_disturb), 'filehelper')
+    elif msg_info['Text'].startswith('delay '):
+        ctrl = msg_info['Text'][6:]
         if ctrl == 'open':
-            append_disturb = True
-            itchat.send(u'打扰模式已开启', 'filehelper')
+            is_delay_rep = True
+            send_msg(u'延时回复已开启', 'filehelper')
         elif ctrl == 'close':
-            append_disturb = False
-            itchat.send(u'打扰模式已关闭', 'filehelper')
-    elif msg['Text'].startswith('log '):
-        ctrl = msg['Text'][4:]
+            is_delay_rep = False
+            send_msg(u'延时回复已关闭', 'filehelper')
+    elif msg_info['Text'].startswith('disturb '):
+        ctrl = msg_info['Text'][8:]
         if ctrl == 'open':
-            record_log = True
-            itchat.send(u'后台记录日志已开启', 'filehelper')
+            is_disturb = True
+            send_msg(u'打扰模式已开启', 'filehelper')
         elif ctrl == 'close':
-            record_log = False
-            itchat.send(u'后台记录日志已关闭', 'filehelper')
-    elif msg['Text'] == 'auther':
-        itchat.send(u'wechat-robot auther:君莫思归', 'filehelper')
-    elif msg['Text'] == 'help':
-        itchat.send(help_info, 'filehelper')
+            is_disturb = False
+            send_msg(u'打扰模式已关闭', 'filehelper')
+    elif msg_info['Text'].startswith('log '):
+        ctrl = msg_info['Text'][4:]
+        if ctrl == 'open':
+            logger.is_record_log = True
+            send_msg(u'后台记录日志已开启', 'filehelper')
+        elif ctrl == 'close':
+            logger.is_record_log = False
+            send_msg(u'后台记录日志已关闭', 'filehelper')
+    elif msg_info['Text'] == 'auther':
+        send_msg(u'wechat-robot auther:君莫思归', 'filehelper')
+    elif msg_info['Text'] == 'help':
+        send_msg(help_info, 'filehelper')
     return True
 
+
 # #图灵机器人已失效
+# KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
+# KEY = 'f26276bebeba492ab763e83e89c511d0'
 # def get_response(msg, userid = 'wechat-robot'):
 #     # 这里我们就像在“3. 实现最简单的与图灵机器人的交互”中做的一样
 #     # 构造了要发送给服务器的数据
@@ -168,15 +226,15 @@ def ctl_msg(msg):
 #         # 将会返回一个None
 #         return
 
-#使用青云客机器人回复，回复速度会慢一些
-def get_response(msg, userid = 'wechat-robot'):
+# 使用青云客机器人回复，回复速度会慢一些
+def get_response(msg_text, userid='wechat-robot'):
     # 这里我们就像在“3. 实现最简单的与图灵机器人的交互”中做的一样
     # 构造了要发送给服务器的数据
     apiUrl = 'http://api.qingyunke.com/api.php'
     params = {
         'key': 'free',
         'appid': '0',
-        'msg': msg
+        'msg': msg_text
     }
     try:
         r = requests.get(apiUrl, params=params).json()
@@ -185,98 +243,112 @@ def get_response(msg, userid = 'wechat-robot'):
         # 将会返回一个None
         return
 
+
 @itchat.msg_register(itchat.content.RECORDING)
-def voice_reply(msg):
-    to_user_nickname = msg['User'].get('NickName', 'unknown')
-    if not auto_rep or (to_user_nickname != 'unknown' and to_user_nickname in no_auto_rep_list):
+def voice_reply(msg_info):
+    to_user_nickname = msg_info['User'].get('NickName')
+    if not rep_mgr.judge_auto_rep(to_user_nickname):
         return
-    if time_interval:
+    if is_delay_rep:
         sec = 6
         print("recive voice. sec", sec)
+
         def repl():
-            itchat.send(u'手机听筒坏了，听不到语音', msg['FromUserName'])
+            send_msg(u'手机听筒坏了，听不到语音', msg_info['FromUserName'])
             print("repled")
+
         t = threading.Timer(sec, repl)
         t.start()
     else:
-        return u'手机听筒坏了，听不到语音'
+        return msg_with_label(u'手机听筒坏了，听不到语音')
+
 
 @itchat.msg_register(itchat.content.PICTURE)
-def picture_reply(msg):
-    if time_interval:
+def picture_reply(msg_info):
+    to_user_nickname = msg_info['User'].get('NickName')
+    if not rep_mgr.judge_auto_rep(to_user_nickname):
+        return
+    if is_delay_rep:
         sec = 2
         print("recive image. sec", sec)
+
         def repl():
-            itchat.send(random.choice(pic_repl_list), msg['FromUserName'])
+            send_msg(random.choice(pic_repl_list), msg_info['FromUserName'])
             print("repled")
+
         t = threading.Timer(sec, repl)
         t.start()
     else:
-        return random.choice(pic_repl_list)
-        
+        return msg_with_label(random.choice(pic_repl_list))
+
+
 # 这里是我们在“1. 实现微信消息的获取”中已经用到过的同样的注册方法
 @itchat.msg_register(itchat.content.TEXT)
-def tuling_reply(msg):
-    global logger, no_auto_rep_list, last_repl_time
-    if ctl_msg(msg):
+def tuling_reply(msg_info):
+    global logger, rep_mgr, last_repl_time
+    if ctl_msg(msg_info):
         return
-    to_user_nickname = msg['User'].get('NickName', 'unknown')
-    if not auto_rep or (to_user_nickname != 'unknown' and to_user_nickname in no_auto_rep_list):
+    to_user_nickname = msg_info['User'].get('NickName')
+    if not rep_mgr.judge_auto_rep(to_user_nickname):
         return
 
     # 为了保证在图灵Key出现问题的时候仍旧可以回复，这里设置一个默认回复
-    defaultReply = 'I received: ' + msg['Text']
+    defaultReply = 'I received: ' + msg_info['Text']
     # 如果图灵Key出现问题，那么reply将会是None
-    reply = get_response(msg['Text'], to_user_nickname)
+    reply = get_response(msg_info['Text'], to_user_nickname)
     # a or b的意思是，如果a有内容，那么返回a，否则返回b
     # 有内容一般就是指非空或者非None，你可以用`if a: print('True')`来测试
     try:
         logger.set_file(to_user_nickname)
-        logger.log('%s:%s' % (to_user_nickname, msg['Text']))
+        logger.log('%s:%s' % (to_user_nickname, msg_info['Text']))
         logger.log('rep:%s' % reply if reply else defaultReply)
     except Exception as e:
         print(str(e))
         print("Exception ignored!!!!")
-        
+
     last_repl_time = datetime.datetime.now()
-    if append_disturb:#todo:性能
+    if is_disturb:
         sec = 600
+
         def repl():
-            if append_disturb and last_repl_time + datetime.timedelta(minutes=9) < datetime.datetime.now():
-                itchat.send(u'怎么不说话了', msg['FromUserName'])
-                print("disturb %s" % msg['FromUserName'])
+            if is_disturb and last_repl_time + datetime.timedelta(minutes=9) < datetime.datetime.now():
+                send_msg(u'怎么不说话了', msg_info['FromUserName'])
+                print("disturb %s" % msg_info['FromUserName'])
+
         t = threading.Timer(sec, repl)
         t.start()
 
-    if time_interval:
+    if is_delay_rep:
         sec = min(len(reply or defaultReply), 50)
         print("sec", sec)
+
         def repl():
-            itchat.send(reply or defaultReply, msg['FromUserName'])
+            send_msg(reply or defaultReply, msg_info['FromUserName'])
             print("repled")
+
         t = threading.Timer(sec, repl)
         t.start()
     else:
-        return reply or defaultReply
+        return msg_with_label(reply or defaultReply)
+
 
 def main():
     # 为了让实验过程更加方便（修改程序不用多次扫码），我们使用热启动
-    #itchat.auto_login(hotReload=True, enableCmdQR=True)
+    # itchat.auto_login(hotReload=True, enableCmdQR=True)
     itchat.auto_login(hotReload=True, enableCmdQR=False)
-    itchat.send('Hello, filehelper', toUserName='filehelper')
+    send_msg('Hello, filehelper', 'filehelper')
     # print itchat.get_chatrooms(update=True)
     itchat.run()
 
+
 if __name__ == '__main__':
-    logger.load_no_auto_rep_list()
+    rep_mgr.load_no_auto_rep_list()
     # while True:
     try:
         main()
         print('end!!!!!')
     except Exception as e:
-        print(str(e))
         print(traceback.format_exc())
         time.sleep(5)
     # print('!!!!!!restart!!!!!!!!')
     print('!!!!!!exit!!!!!!!!')
-
